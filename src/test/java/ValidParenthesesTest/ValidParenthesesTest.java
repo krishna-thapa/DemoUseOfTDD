57cd78ca9816d1ae9f01d8435d41a9bf1db18f14
Krishna Thapa <krishna.nature91@gmail.com>
2019-01-19 19:45:58 +0000

    Valid Parentheses algorithm with HashMap and Stack added

--- src/test/java/ValidParenthesesTest/ValidParenthesesTest.java
+++ src/test/java/ValidParenthesesTest/ValidParenthesesTest.java
@@ -8,6 +8,15 @@ import org.junit.Test;
 
 public class ValidParenthesesTest {
 
+    /**
+     * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     *
+     * An input string is valid if:
+     *
+     *     Open brackets must be closed by the same type of brackets.
+     *     Open brackets must be closed in the correct order.
+     */
+
     private ValidParentheses validParentheses;
 
     @Before
